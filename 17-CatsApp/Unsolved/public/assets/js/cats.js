8cc3594024021f1dd71e73c0ea5838cf80be3528
Rachel Moore <moorer@gcsnc.com>
2020-12-15 19:37:50 -0500

    added read me

--- 17-CatsApp/Unsolved/public/assets/js/cats.js
+++ 17-CatsApp/Unsolved/public/assets/js/cats.js
@@ -3,13 +3,13 @@ $(function() {
   $(".change-sleep").on("click", function(event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
-console.log(newSleep)
+
     var newSleepState = {
-      sleepy: !newSleep
+      sleepy: newSleep
     };
 
     // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
+    $.ajax("/api/cats/" + id, {
       type: "PUT",
       data: newSleepState
     }).then(
@@ -25,34 +25,18 @@ console.log(newSleep)
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newBurger = {
+    var newCat = {
       name: $("#ca").val().trim(),
-      sleepy: 0
-      // $("[name=sleepy]:checked").val().trim()
+      sleepy: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/burgers", {
+    $.ajax("/api/cats", {
       type: "POST",
-      data: newBurger
+      data: newCat
     }).then(
       function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
-
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
+        console.log("created new cat");
         // Reload the page to get the updated list
         location.reload();
       }
